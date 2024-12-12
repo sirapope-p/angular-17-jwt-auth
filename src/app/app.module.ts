434ac11de37b2dedcea231b23b05c072f96f0584
Good <sirapope@profile.co.th>
2024-12-12 22:51:03 +0700

    add component in _helper and edit service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +12,8 @@ import { ProfileComponent } from './profile/profile.component';
 import { BoardAdminComponent } from './board-admin/board-admin.component';
 import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
 import { BoardUserComponent } from './board-user/board-user.component';
+
+import { httpInterceptorProviders } from './_helpers/http.interceptor';
 
 @NgModule({
   declarations: [
@@ -24,9 +28,45 @@ import { BoardUserComponent } from './board-user/board-user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [httpInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
+// import { HomeComponent } from './home/home.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { BoardAdminComponent } from './board-admin/board-admin.component';
+// import { BoardModeratorComponent } from './board-moderator/board-moderator.component';
+// import { BoardUserComponent } from './board-user/board-user.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent,
+//     RegisterComponent,
+//     HomeComponent,
+//     ProfileComponent,
+//     BoardAdminComponent,
+//     BoardModeratorComponent,
+//     BoardUserComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
